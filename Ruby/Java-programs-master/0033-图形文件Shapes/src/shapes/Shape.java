5409ca37d4fa7fd2aeb8e098c7e2e2f1ed4c647c
闁告鍟撮崳鐑藉礃閺夎法娈± <zhangyinsen@126.com>
2016-05-28 19:01:09 +0800

    ~

--- Ruby/Java-programs-master/0033-图形文件Shapes/src/shapes/Shape.java
+++
@@ -1,9 +0,0 @@
-package shapes;
-
-import java.awt.Graphics;
-
-public abstract class Shape {
-	
-	public abstract void draw(Graphics g);
-	
-}
--- Ruby/Java-programs-master/foxnrabbit/src/cell/Cell.java
+++
@@ -1,7 +0,0 @@
-package cell;
-
-import java.awt.Graphics;
- 
-public interface Cell {
-	void draw(Graphics g, int x, int y, int size);
-}
