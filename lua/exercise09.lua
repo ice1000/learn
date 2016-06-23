08e1096d695e4b3ed90991d8288d3f7eee2c8d58
ice1000 <951394653@qq.com>
2016-06-23 10:14:44 +0800

    怎么跟教程上说的不一样啊

--- lua/exercise09.lua
+++ lua/exercise09.lua
@@ -1,2 +1,13 @@
 print("load finished.")
 print("111\n111\t111\"")
+
+
+
+for i = 0, 100, 1 do
+  --[ print(random(0, i)) --]
+end
+
+sleep(100)
+vibrate(233)
+
+--[ Not the same like the tutorial said --]
