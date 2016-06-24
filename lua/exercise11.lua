6a77480a586c681d03795ad30d95378203b48434
ice1000 <951394653@qq.com>
2016-06-24 16:25:07 +0800

    so much fun, it's OOP.

--- lua/exercise11.lua
+++ lua/exercise11.lua
@@ -1,9 +1,9 @@
 print(os.date())
 
 Person = {
-  birth = os.date()
-  name = ""
-  age = 1
+  birth = os.date(),
+  name = "",
+  age = 1,
   gender = 1
 }
 
@@ -12,5 +12,9 @@ function Person: new(o, name, age, gender)
   setmetatable(o, self)
   self.__index = self
   self.birth = os.date()
-  
+  self.name = name or ""
+  self.age = age or 1
+  self.gender = gender or 0
 end
+
+ice = Person:new(nil, "ice1000", 16, 3)
