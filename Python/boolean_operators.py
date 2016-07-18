45089e564fe4d110bfbf28977d8d830f84fd13d2
闁绘瑦澹嗙€氭鎯冮崟顐㈢．闂佹彃鑻崯铏逛焊鑱 <ice1000kotlin@foxmail.com>
2016-07-18 19:02:54 +0800

    The King of Python

---
+++ Python/boolean_operators.py
@@ -0,0 +1,6 @@
+name = "John"
+age = 17
+
+print(name == "John" or age == 17)    # checks that either name equals to "John" OR age equals to 17
+
+print(name.__eq__("John") and age != 23)
---
+++ Python/string_formatting.py
@@ -0,0 +1,4 @@
+name = "John"
+print("Hello, PyCharm! My name is %s!" % name)     # Note: %s is inside the string, % is after the string
+years = 16
+print("I'm %d years old" % years)
