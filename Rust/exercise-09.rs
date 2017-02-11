cb7a078dc6bbb82f43807aae0ce7760b547d8ee3
ice1000 <ice1000kotlin@foxmail.com>
2017-02-11 17:18:32 +0800

    commit via a script

---
+++ Rust/exercise-09.rs
@@ -0,0 +1,44 @@
+#![allow(non_snake_case, non_camel_case_types)]
+
+extern crate jni;
+
+//use jni::JNIEnv;
+use jni::sys::*;
+use jni::objects::{JClass, JObject, JValue};
+
+//#[no_mangle]
+//pub extern fn Java_jni_Main_newIntArray(
+//	env: JNIEnv,
+//	jc: jclass,
+//	len: jint) -> ! {
+////	return env.NewIntArray(len);
+//}
+
+#[no_mangle]
+pub extern fn Java_jni_Main_getSquared(
+	env: JNIEnv,
+	jc: jclass,
+	origin: jint) -> jint {
+	origin * origin
+}
+
+//noinspection RsFunctionNaming
+#[no_mangle]
+pub extern fn Java_jni_Main_getSqrt(
+	env: *mut JNIEnv,
+	jc: jclass,
+	origin: jint) -> jint {
+	let mut left = 0;
+	let mut right = origin;
+	while right - left > 1 {
+		let mid = (left + right) >> 1;
+		if mid * mid >= origin {
+			right = mid;
+		} else {
+			left = mid;
+		}
+	}
+	right
+}
+
+//fn main() {}
