f2ecfaaf0323544fcd2289e268caaaf751686829
ice1000 <ice1000kotlin@foxmail.com>
2017-05-16 02:51:49 +0800

    update

---
+++ CodeWars/Haskell/escape-the-mines.hs
@@ -0,0 +1,24 @@
+module EscapeTheMines where
+
+import Control.Applicative
+
+type XY = (Int, Int)
+data Move = U | D | R | L deriving (Eq, Show)
+
+dfs :: [[Bool]] -> XY -> XY -> Maybe [Move]
+dfs m s e
+  |s == e            = Just []
+  |x < 0 || y < 0    = Nothing
+  |x >= len m        = Nothing
+  |y >= len (m !! x) = Nothing
+  |not $ m !! x !! y = Nothing
+  |otherwise = (R:) <$> dfs m (x + 1, y) e
+           <|> (D:) <$> dfs m (x, y + 1) e
+           <|> (L:) <$> dfs m (x - 1, y) e
+           <|> (U:) <$> dfs m (x, y - 1) e
+   where (x, y) = s
+         len = length
+--
+
+solve :: [[Bool]] -> XY -> XY -> [Move] 
+solve a b c = case dfs a b c of Just res -> res
