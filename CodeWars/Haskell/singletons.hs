71d8eb68d82b99179b730a077c4b5c297fbe8459
ice1000 <ice1000kotlin@foxmail.com>
2017-05-27 13:33:24 +0800

    all hail dependent type

--- CodeWars/Haskell/singletons.hs
+++ CodeWars/Haskell/singletons.hs
@@ -12,7 +12,16 @@
 
 module Singletons where
 
-import Prelude hiding (drop, take, head, tail, index, zipWith, replicate, map, (++))
+import Prelude hiding (drop,
+                       take,
+                       head,
+                       tail,
+                       index,
+                       zipWith,
+                       replicate,
+                       map,
+                       (++))
+--
 
 data Vec a n where
   VNil :: Vec a Zero
@@ -20,7 +29,7 @@ data Vec a n where
 --
 
 -- promoted to type level by data kinds
-data Nat = Zero | Succ Nat
+data Nat = Zero | Succ Nat deriving (Show)
 
 data SNat a where
   SZero :: SNat Zero
@@ -33,12 +42,23 @@ type instance Zero :< Succ n = True
 type instance (Succ m) :< (Succ n) = m :< n
 
 type family (Add (a :: Nat) (b :: Nat)) :: Nat
-type instance (Add Zero m) = m
-type instance (Add (Succ n) m) = Add n (Succ m)
+type instance Add Zero m     = m
+type instance Add (Succ n) m = Succ (Add n m)
+
+-- question: why I cannot use wildcard???
+type family (Sub (a :: Nat) (b :: Nat)) :: Nat
+type instance Sub m Zero            = m
+type instance Sub Zero m            = Zero
+type instance Sub (Succ n) (Succ m) = Sub n m
+
+type family (Min (a :: Nat) (b :: Nat)) :: Nat
+type instance Min m Zero            = Zero
+type instance Min Zero m            = Zero
+type instance Min (Succ n) (Succ m) = Succ (Min n m)
 
 map :: (a -> b) -> Vec a n -> Vec b n
 map f VNil = VNil
-map f (VCons x xs) = VCons (f x) (map f xs)
+map f (VCons x xs) = VCons (f x) $ map f xs
 
 index :: ((a :< b) ~ True) => SNat a -> Vec s b -> s
 index SZero (VCons v _) = v
@@ -54,27 +74,36 @@ zipWith _ VNil VNil = VNil
 zipWith f (VCons v l) (VCons v2 l2) = VCons (f v v2) $ zipWith f l l2
 
 (++) :: Vec v m -> Vec v n -> Vec v (Add m n)
-VNil ++ VNil = VNil
-VNil ++ (VCons x y) = (VCons x y)
-(++) (VCons a b) (VCons x y) = (VCons x (VCons a b)) ++ y
+VNil ++ ls        = ls
+(VCons a b) ++ ls = VCons a (b ++ ls)
 
 -- The semantics should match that of take for normal lists.
-take :: SNat n -> Vec a m -> Vec a n
-take SZero (VCons a m)         = VNil
-take (SSucc SZero) (VCons a m) = VCons a VNil
-take (SSucc n) (VCons a m)     = VCons a $ take n m
+take :: SNat n -> Vec a m -> Vec a (Min m n)
+take _     VNil            = VNil
+take SZero _               = VNil
+take (SSucc n) (VCons a m) = VCons a $ take n m
 
 -- The semantics should match that of drop for normal lists.
-drop :: SNat n -> Vec a m -> Vec a o
--- drop SZero survivor            = survivor
--- drop (SSucc SZero) (VCons a m) = m
--- drop (SSucc n) (VCons a m)     = drop n m
+drop :: SNat n -> Vec a m -> Vec a (Sub m n)
+drop _     VNil            = VNil
+drop SZero survivor        = survivor
+drop (SSucc n) (VCons a m) = drop n m
 
 head :: Vec a n -> a
 head (VCons a n) = a
 
-tail :: Vec a n -> a
-tail (VCons a VNil) = a
-tail (VCons a xs)   = tail xs
+tail :: Vec a (Succ n) -> Vec a n
+tail (VCons _ m) = m
 
+-- Fuck dependent type
+
+numberOne = Succ Zero
+numberTwo = Succ numberOne
+numberThree = Succ numberTwo
+numberFour = Succ numberThree
+numberFive = Succ numberFour
+numberSix = Succ numberFive
+numberSeven = Succ numberSix
+
+exampleTestList = VCons numberFive $ VCons numberSix $ VCons numberTwo $ VCons numberFour $ VCons numberSeven VNil
 
