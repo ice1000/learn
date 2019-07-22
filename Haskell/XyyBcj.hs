0891af354163315792e97c99a4d23069a1941a7c
ice1000 <ice1000kotlin@foxmail.com>
2019-07-22 02:01:56 -0700

    This might be wrong

--- Haskell/XyyBcj.hs
+++ Haskell/XyyBcj.hs
@@ -1,24 +1,42 @@
-module XyyBcj where
+{-# LANGUAGE Strict #-}
+module Main where
 
-import Control.Monad
-import Control.Monad.State
-import Data.Vector ((!), (//), Vector)
+import           Control.Monad
+import qualified Data.Vector.Unboxed.Mutable as V
+import           GHC.Prim                    (RealWorld)
 
--- import Data.List.Split
+type UnionFind = V.MVector RealWorld Int
 
-type UFind  = Vector Int
-type UFindM = State UFind
+ask :: UnionFind -> Int -> IO (UnionFind, Int)
+ask vec i = do
+  papa <- V.read vec i
+  if papa == i then pure (vec, i)
+  else do
+    (vec', mama) <- ask vec papa
+    V.write vec' i mama
+    pure (vec', mama)
 
-ask :: Int -> UFindM Int
-ask i = do
-  parent <- (! i) <$> get
-  when (parent /= i) $ do
-    root <- ask parent
-    modify (// pure (i, root))
-  (! i) <$> get
+cat :: UnionFind -> Int -> Int -> IO UnionFind
+cat vec x y = do
+  (vec' , x') <- ask vec  x
+  (vec'', y') <- ask vec' y
+  V.write vec'' x' y'
+  pure vec''
 
-cat :: Int -> Int -> UFindM ()
-cat x y = do
-  xRoot <- ask x
-  yRoot <- ask y
-  modify (// pure (xRoot, yRoot))
+resolve :: Int -> UnionFind -> IO ()
+resolve 0 _ = pure ()
+resolve n uf = do
+  [z, x, y] <- map read . words <$> getLine :: IO [Int]
+  uf' <- if z == 1 then cat uf x y else do
+    (vec , x') <- ask uf  x
+    (vec', y') <- ask vec y
+    putStrLn $ if x' == y' then "Y" else "N"
+    pure vec'
+  resolve (n - 1) uf'
+
+main :: IO ()
+main = do
+  [n, m] <- map read . words <$> getLine
+  vector <- V.replicate (n + 1) (0 :: Int)
+  forM_ [0 .. n] $ \i -> V.write vector i i
+  resolve m vector
