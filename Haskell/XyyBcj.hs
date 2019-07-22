44124dff7b1fe8e85dba9e7c07f603a354650c1b
ice1000 <ice1000kotlin@foxmail.com>
2019-07-22 14:07:06 -0700

    Read opt, -O2

--- Haskell/XyyBcj.hs
+++ Haskell/XyyBcj.hs
@@ -1,13 +1,21 @@
 {-# LANGUAGE Strict #-}
+{-# GHC-OPTIONS -O2 #-}
 module Main where
 
 import           Control.Monad
+import           Data.Char
 import qualified Data.Text                   as T
 import qualified Data.Text.IO                as I
 import qualified Data.Vector.Unboxed.Mutable as V
 import           GHC.Prim                    (RealWorld)
 
 type UnionFind = V.MVector RealWorld Int
+
+int :: String -> Int
+int s = int' s 0
+  where
+    int' [    ] n = n
+    int' (c:cs) n = int' cs $ n * 10 + (ord c - ord '0')
 
 ask :: UnionFind -> Int -> IO Int
 ask vec i = do
@@ -27,7 +35,7 @@ cat vec x y = do
 resolve :: Int -> UnionFind -> IO ()
 resolve 0 _ = pure ()
 resolve n uf = do
-  [z, x, y] <- map (read . T.unpack) . T.words <$> I.getLine
+  [z, x, y] <- map (int . T.unpack) . T.words <$> I.getLine
   if z == 1 then cat uf x y else do
     x' <- ask uf x
     y' <- ask uf y
@@ -37,7 +45,7 @@ resolve n uf = do
 
 main :: IO ()
 main = do
-  [n, m] <- map (read . T.unpack) . T.words <$> I.getLine
+  [n, m] <- map (int . T.unpack) . T.words <$> I.getLine
   vector <- V.replicate (n + 1) (0 :: Int)
   forM_ [0 .. n] $ \i -> V.write vector i i
   resolve m vector
